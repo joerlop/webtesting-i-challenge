89d1037bc13bc74e9f9baea9c828b1ee337a5269
joerlop <jj.erlich155@gmail.com>
2019-06-17 15:59:00 -0500

    Project done

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,7 +1,34 @@
 module.exports = {
   repair,
+  success,
+  fail
 };
 
 function repair(item) {
   return { ...item, durability: 100 };
+}
+
+function success(item) {
+  const enhancement = item.enhancement;
+
+  if (enhancement == 20) {
+    return { ...item };
+  } else {
+    return { ...item, enhancement: enhancement + 1 }
+  }
+}
+
+function fail(item) {
+  const enhancement = item.enhancement;
+  const durability = item.durability;
+
+  if (enhancement < 15) {
+      return { ...item, durability: Math.max(0, durability - 5) };
+  } else {
+    if (enhancement > 16) {
+      return { enhancement: enhancement - 1, durability: Math.max(0, durability - 10) };
+    } else {
+      return { ...item, durability: Math.max(0, durability - 10) };
+    }
+  }
 }
